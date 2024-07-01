64fbddb96f73755ab4c6f170a13114273e09f9e9
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-07-01 14:27:16 +0700

    fix: fix replication on gameplay ability system on multiplayer mode

--- Source/UnrealMultiplayer/RuneAttributeSet.h
+++ Source/UnrealMultiplayer/RuneAttributeSet.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// RuneAttributeSet.h
 
 #pragma once
 
@@ -24,7 +24,7 @@ public:
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 	// Health
-	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Attributes")
 	FGameplayAttributeData Health;
 	ATTRIBUTE_ACCESSORS(URuneAttributeSet, Health);
 
@@ -32,15 +32,15 @@ public:
 	virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
 
 	// Mana
-	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Attributes")
 	FGameplayAttributeData Mana;
 	ATTRIBUTE_ACCESSORS(URuneAttributeSet, Mana);
 
 	UFUNCTION()
-	virtual void OnRep_Mana(const FGameplayAttributeData& OldStamina);
+	virtual void OnRep_Mana(const FGameplayAttributeData& OldMana);
 
 	// Stamina
-	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Stamina, Category = "Attributes")
 	FGameplayAttributeData Stamina;
 	ATTRIBUTE_ACCESSORS(URuneAttributeSet, Stamina);
 
@@ -48,7 +48,7 @@ public:
 	virtual void OnRep_Stamina(const FGameplayAttributeData& OldStamina);
 
 	// Vigor
-	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Vigor, Category = "Attributes")
 	FGameplayAttributeData Vigor;
 	ATTRIBUTE_ACCESSORS(URuneAttributeSet, Vigor);
 
@@ -56,7 +56,7 @@ public:
 	virtual void OnRep_Vigor(const FGameplayAttributeData& OldVigor);
 
 	// Mind
-	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mind, Category = "Attributes")
 	FGameplayAttributeData Mind;
 	ATTRIBUTE_ACCESSORS(URuneAttributeSet, Mind);
 
@@ -64,7 +64,7 @@ public:
 	virtual void OnRep_Mind(const FGameplayAttributeData& OldMind);
 
 	// Endurance
-	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Endurance, Category = "Attributes")
 	FGameplayAttributeData Endurance;
 	ATTRIBUTE_ACCESSORS(URuneAttributeSet, Endurance);
 
@@ -72,7 +72,7 @@ public:
 	virtual void OnRep_Endurance(const FGameplayAttributeData& OldEndurance);
 
 	// Strength
-	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Strength, Category = "Attributes")
 	FGameplayAttributeData Strength;
 	ATTRIBUTE_ACCESSORS(URuneAttributeSet, Strength);
 
@@ -80,7 +80,7 @@ public:
 	virtual void OnRep_Strength(const FGameplayAttributeData& OldStrength);
 
 	// Dexterity
-	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Dexterity, Category = "Attributes")
 	FGameplayAttributeData Dexterity;
 	ATTRIBUTE_ACCESSORS(URuneAttributeSet, Dexterity);
 
@@ -88,7 +88,7 @@ public:
 	virtual void OnRep_Dexterity(const FGameplayAttributeData& OldDexterity);
 
 	// Intelligence
-	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Intelligence, Category = "Attributes")
 	FGameplayAttributeData Intelligence;
 	ATTRIBUTE_ACCESSORS(URuneAttributeSet, Intelligence);
 
@@ -96,7 +96,7 @@ public:
 	virtual void OnRep_Intelligence(const FGameplayAttributeData& OldIntelligence);
 
 	// Faith
-	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Faith, Category = "Attributes")
 	FGameplayAttributeData Faith;
 	ATTRIBUTE_ACCESSORS(URuneAttributeSet, Faith);
 
@@ -104,7 +104,7 @@ public:
 	virtual void OnRep_Faith(const FGameplayAttributeData& OldFaith);
 
 	// Luck
-	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Luck, Category = "Attributes")
 	FGameplayAttributeData Luck;
 	ATTRIBUTE_ACCESSORS(URuneAttributeSet, Luck);
 
